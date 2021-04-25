c2492452145bbb475a1c235e5e369d136b401ad9
hannes_remote <33062605+HannesGitH@users.noreply.github.com>
2021-04-25 19:17:41 +0200

    stupid idea

--- A02/Decoder.py
+++ A02/Decoder.py
@@ -1,3 +1,5 @@
+import numpy as np
+
 class Decoder:
 
     def __init__(self):
@@ -19,10 +21,42 @@ class Decoder:
 
         file = open(path, "wb")
         file.write(self.decoded_data)
+        file.close()
         return True
 
     def decode(self,encoded_stream):
 
-        # actual decoding
-        self.decoded_data=encoded_stream
-        return self.decoded_data
+        #check whether the wrong files are decoded
+        assert encoded_stream[:8] == b'IVC_SS21'
+
+        #version check for backwards compatibility
+        if encoded_stream[8:13] == b'v0001':
+            print("decoding version 0001 ..")
+
+            # get metadata
+            block_size = int.from_bytes(encoded_stream[13:15],"big")
+            blocks_x = int.from_bytes(encoded_stream[15:17],"big")
+            blocks_y = int.from_bytes(encoded_stream[17:19],"big")
+
+            # retreiving blocks from binary
+            
+            blocks = np.zeros([blocks_x,blocks_y,block_size,block_size])
+            counter = 0
+            current_block = np.zeros([block_size^2])
+            for byte in encoded_stream[19:]:
+                if counter%(block_size^2) == 0:
+                    blocks[counter/block_size,(counter/block_size)%blocks_x]
+                    counter = 0
+                    continue
+                current_block[counter]=byte
+                counter += 1
+
+            # actual decoding
+            pgm_image_data=b''
+
+            pgm_metadata = f'P5\n{blocks_x_count*block_size} {blocks_y_count*block_size}\n255\n'.encode()
+
+            self.decoded_data = pgm_metadata + pgm_image_data
+            return self.decoded_data
+
+        raise Exception("Version not Found")
--- A02/main.py
+++ A02/main.py
@@ -3,13 +3,17 @@ from Decoder import Decoder as Dec
 
 def main():
     filepath_raw = "/home/hannes/uni/video_enc/ivc-ss21/A01/pgm/Berlin.pgm" or input("file to transcode: ")
+    filepath_encoded = "/home/hannes/uni/video_enc/ivc-ss21/out/Berlin.ivc21" or input("encoded path: ")
     filepath_transcoded = "/home/hannes/uni/video_enc/ivc-ss21/A01/pgm/Berlin_transcoded.pgm" or input("file to output: ")
 
     enc = Enc()
     dec = Dec()
 
     encoded_stream = enc(filepath_raw)
-    print(f"encoded stream : {type(encoded_stream)}")
+
+    enc_file = open(filepath_encoded,"wb")
+    enc_file.write(encoded_stream)
+    enc_file.close()
 
     dec(encoded_stream,path=filepath_transcoded)
 
