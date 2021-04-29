11aa3d35d1e18b156cdd7af2ddd07746364b4fe5
marisanest <marisanest@mailbox.org>
2021-04-29 18:37:41 +0200

    Add PSNR test.

---
+++ test.py
@@ -0,0 +1,22 @@
+import argparse
+from tests.psnr import *
+
+
+def parse_args():
+    parser = argparse.ArgumentParser(description='Tests encoder and decoder quality with respect to an original JPEG codec.')
+    parser.add_argument('-f', alias='--filename', dest='filename', type=str, help='filename of input image without full path and suffix, e.g. Berlin', required=True)
+    parser.add_argument('-v', alias='--version', dest='version', type=str, help='version of new encoder and decoder, e.g. \'1.0\'', required=True)
+    parser.add_argument('-vs', alias='--versions', dest='versions', type=str, help='comma separated versions that should be also compared to the new encoder and decoder version, e.g. \'1.0,2.0\'', required=True)
+
+    args = parser.parse_args()
+    return args
+
+
+def main():
+    args = parse_args()
+    generate_data(args.filename, args.version)
+    plot_data(args.filename, [args.versions, args.version])
+
+
+if __name__ == '__main__':
+    main()
---
+++ tests/psnr.py
@@ -0,0 +1,57 @@
+import subprocess
+import pandas as pd
+import os
+import datetime
+
+from Encoder import Encoder as Encoder
+from Decoder import Decoder as Decorder
+
+PSNR_TOOL_PATH = 'tools/psnr-images/bin/psnrImg'
+
+DATA_ROOT_PATH = 'dat'
+
+IMAGES_ROOT_PATH = 'images'
+PGM_ORIGINAL_PATH = os.path.join(IMAGES_ROOT_PATH, 'original')
+BITSTREAM_PATH = os.path.join(IMAGES_ROOT_PATH, 'bitstream')
+PGM_RECONSTRUCTION_PATH = os.path.join(IMAGES_ROOT_PATH, 'reconstruction')
+
+PGM_SUFFIX = '.pgm'
+BITSTREAM_SUFFIX = '.jpeg'  # todo: check suffix with other groups
+DATA_SUFFIX = '.dat'
+
+
+def generate_data(filename, version):
+    input_path = os.path.join(PGM_ORIGINAL_PATH, filename + PGM_SUFFIX)
+    bitstream_path = os.path.join(BITSTREAM_PATH + BITSTREAM_SUFFIX, filename)
+    output_path = os.path.join(PGM_RECONSTRUCTION_PATH, filename + PGM_SUFFIX)
+
+    df = pd.DataFrame(columns=['quality', 'bpp', 'db'])
+
+    for index, quality in enumerate([8, 12, 16, 20, 24]):
+        Encoder()(input_path, bitstream_path, quality)  # todo: check interface with other groups
+        Decorder()(bitstream_path, output_path)  # todo: check interface with other groups
+
+        process = subprocess.run(
+            [PSNR_TOOL_PATH, input_path, bitstream_path, output_path],
+            stdout=subprocess.PIPE
+        )
+
+        stdout = process.stdout.decode("utf-8")
+        bpp, db = stdout.split(' bpp ')
+        bpp, db = float(bpp), float(db.replace(' dB', ''))
+
+        df.loc[index] = [bpp, db]
+
+    version_path = os.path.join(DATA_ROOT_PATH, version)
+    os.mkdir(version_path)
+    df.to_pickle(os.path.join(version_path, filename + DATA_SUFFIX))
+
+
+def plot_data(filename, versions):
+    df = pd.DataFrame(columns=['quality', 'bpp', 'db'], index=['jpeg'] + versions)
+    df.loc['jpeg'] = None  # todo: implement
+
+    for version in versions:
+        df.loc[version] = pd.read_pickle(os.path.join(DATA_ROOT_PATH, version, filename))
+
+    # todo: generate multiline plot and save
