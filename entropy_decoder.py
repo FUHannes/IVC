07a953e95dde7595e45f20e2d7c49237085037bf
hannes_remote <33062605+HannesGitH@users.noreply.github.com>
2021-05-06 18:43:55 +0200

    exercise 04 team entropy decoder

--- entropy_decoder.py
+++ entropy_decoder.py
@@ -1,25 +1,57 @@
+# abstraction for an input bitstream 
+class IBitstream:
+
+    def __init__( self, byteArray):
+        pass
+    # get next bit from bitstream
+    def getBit( self ):
+        pass
+    # get next bits from bitstream (returns bit pattern, most significant bit first)
+    def getBits( self, numBits: int ):
+        pass    
+    # read bits until end of byte
+    def byteAlign( self ):
+        pass
+
+
+
+
+# class for all the entropy decoding
 class EntropyDecoder:
-    def __init__ ( self , bitstream ):
+
+    def __init__( self, bitstream: IBitstream ):
         self.bitstream = bitstream
+
+    def readQIndexBlock( self, blockSize: int ):
+        # loop over all positions inside NxN block
+        #  --> call readQIndex for all quantization index
+        out_integer_array = []
+        for index in range(blockSize**2):
+            out_integer_array.append(self.readQIndex())
+        return out_integer_array
+
+    def readQIndex( self ):
+        # (1) read expGolomb for absolute value
+        # (2) read sign bit for absolutes values > 0
+        # (3) return value
+        value = self.expGolomb()
+        return value * (-1 if self.bitstream.getBit() else 1)
+
         
-    def readLevel ( self ):
-
-        out_integer_array=[]
-
+    def expGolomb( self ):
+        # (1) read class index k using unary code (read all bits until next '1'; classIdx = num zeros)
+        # (2) read position inside class as fixed-length code of k bits [red bits]
+        # (3) return value
         current_red_code=1
         reading_blue_part = True
         red_length = 0
-        for bit in self.bitstream:
+        while True:
+            bit = int(self.bitstream.getBit())
             if reading_blue_part:
                 red_length+=1
-                reading_blue_part = not bit
+                reading_blue_part = (bit == 0)
             else:
-                current_red_code * 2 + int(bit) #ok der teil wäre wohl in c wesentlich schneller (current_red_code << 1 + bit)
+                current_red_code = current_red_code * 2 + int(bit) #ok der teil wäre wohl in c wesentlich schneller (current_red_code << 1 + bit)
                 red_length-=1
                 if red_length == 0:
-                    out_integer_array.append(current_red_code-1)
-                    current_red_code=1
-                    reading_blue_part = True
-
-        return out_integer_array #maybe as bytes or idk
-
+                    return current_red_code-1
