7b2138af758d1946baa3a4bf3b7100af1411abed
lillijo <lillijoppien@gmail.com>
2021-05-06 10:12:05 +0200

    decoder rewrite

--- Decoder.py
+++ Decoder.py
@@ -1,88 +1,44 @@
 import numpy as np
-# !pip install joblib
-from joblib import Parallel, delayed
-from functools import reduce
+import EntropyDecoder
+import IBitstream
+
 
 class Decoder:
 
-    def __init__(self,multithreaded=True):
-        self.multithreaded=multithreaded
+    def __init__(self, input_path, output_path):
+        self.output_path = output_path
+        self.bitstream = IBitstream(input_path)
+        self.image_width = int(self.bitstream.getBits(16))
+        self.image_height = int(self.bitstream.getBits(16))
+        self.block_size = int(self.bitstream.getBits(16))
+        self.qp = int(self.bitstream.getBits(8))
+        self.qs = 2**(self.qp/4)
+        self.blocks_width = self.image_width / self.block_size
+        self.blocks_height = self.image_height / self.block_size
+        self.image = np.zeros([self.image_width, self.image_height, self.block_size, self.block_size], dtype=np.int8)
+        self.ent_dec = EntropyDecoder(self.bitstream)
 
-    #automatic pipeline
-    def __call__(self,input_path,output_path=None):
-        self.read_in(input_path)
-        self.decode()
-        if output_path is not None:
-            self.write_out(output_path)
-        return self.decoded_stream
+    def decode_block(self, x: int, y: int):
+        ent_dec_block = self.ent_dec.readQIndexBlock(self.block_size)
+        ent_dec_block = ent_dec_block.reshape([self.block_size, self.block_size])
+        dequantized_block = ent_dec_block * self.qs
+        self.image[x][y] = np.clip(dequantized_block + 128, 0, 255).astype('uint8')
+        # entropy decoding (EntropyDecoder)
+        # de-quantization
+        # adding prediction (128)
+        # clipping (0,255)
 
-    #opening and reading a binary file
-    def read_in(self,input_path):
-        in_file = open(input_path, "rb")
-        self.raw_bytes = in_file.read()
-        in_file.close()
-        return self.raw_bytes
-
-    #opening and writing a binary file
-    def write_out(self,output_path):
-        out_file = open(output_path, "wb")
-        out_file.write(self.decoded_stream)
+    # opening and writing a binary file
+    def write_out(self):
+        out_file = open(self.output_path, "wb")
+        pgm_metadata = f'P5\n{self.image_width} {self.image_height}\n255\n'.encode()
+        pgm_image_data = np.swapaxes(self.image, 1, 2).ravel().tobytes()
+        decoded_stream = pgm_metadata + pgm_image_data
+        out_file.write(decoded_stream)
         out_file.close()
         return True
 
-    
-    def decode(self,raw_bytes=None):
-
-        if (raw_bytes is not None):
-            self.raw_bytes = raw_bytes
-
-        #check whether the wrong files are decoded
-        assert self.raw_bytes[:8] == b'IVC_SS21'
-
-        self.version = int(self.raw_bytes[9:13].decode("ascii"))
-        print(f"decoding version {self.version} ..")
-
-        if True: #possible version switching for incompatible header format here
-            # get metadata
-            self.block_size = int.from_bytes(self.raw_bytes[13:15],"big")
-            blocks_x = int.from_bytes(self.raw_bytes[15:17],"big")
-            blocks_y = int.from_bytes(self.raw_bytes[17:19],"big")
-
-            # retreiving blocks from binary                              
-            encoded_blocks = np.frombuffer(self.raw_bytes[19:],dtype=np.uint8).reshape([blocks_x,blocks_y,-1])
-
-            #setup empty blocks
-            decoded_blocks = np.zeros([blocks_x,blocks_y,self.block_size,self.block_size],dtype=np.uint8)
-
-            if self.multithreaded:
-                #multithreaded block decoding
-                def thread_task(encoded_block_stream):
-                    decoded_block_stream=np.zeros([blocks_y,self.block_size,self.block_size],dtype=np.uint8)
-                    for yi in range(blocks_y):
-                        decoded_block_stream[yi] = self._decode_block_(encoded_block_stream[yi])
-                    return decoded_block_stream
-
-                decoded_blocks = Parallel(n_jobs=blocks_x, backend="threading")(map(delayed(thread_task), encoded_blocks))
-
-            else:
-                for xi in range(blocks_x):
-                    for yi in range(blocks_y):
-                        decoded_blocks[xi,yi] = self._decode_block_(encoded_blocks[xi,yi,:])
-
-            #deblock and convert to bytestream
-            pgm_image_data = np.swapaxes(decoded_blocks,1,2).ravel().tobytes()
-
-        pgm_metadata = f'P5\n{blocks_x*self.block_size} {blocks_y*self.block_size}\n255\n'.encode()
-
-        self.decoded_stream = pgm_metadata + pgm_image_data
-        return self.decoded_stream
-
-
-    def _decode_block_(self,block):
-
-        #version check for backwards compatibility
-        if self.version == 1:
-            block = block.reshape([self.block_size,self.block_size])#not much to decode in version 1
-            return block
-
-        raise Exception("incompatible version")
+    def decode_image(self):
+        for xi in range(self.blocks_width):
+            for yi in range(self.blocks_height):
+                self.decode_block_(xi, yi)
--- decode.py
+++ decode.py
@@ -1,23 +1,23 @@
 import time
 import argparse
 
-from Decoder import Decoder as Dec
-
+from Decoder import Decoder
 
 
 def main():
     parser = argparse.ArgumentParser(description='PGM decoder')
-    requiredNamed = parser.add_argument_group('required arguments')
-    requiredNamed.add_argument('-o', '--output', help='reconstructed image in pgm format', required=True,dest="output")
-    requiredNamed.add_argument('-b', '--bitstream', help='bitstream file to be read by decoder', required=True, dest="bitstream")
+    required_named = parser.add_argument_group('required arguments')
+    required_named.add_argument('-b', '--bitstream', help='bitstream file to be read by decoder',
+                                required=True, dest="bitstream")
+    required_named.add_argument('-o', '--output', help='reconstructed image in pgm format',
+                                required=True, dest="output")
     args = parser.parse_args()
 
-    dec = Dec()
+    dec = Decoder(args.bitstream, args.output)
 
-    start_time = time.process_time()                        #benchmarking speed
-    dec(args.bitstream,args.output)                         #decoding
+    start_time = time.process_time()  # benchmarking speed
+    dec.decode_block()
     decoding_time = time.process_time() - start_time   
-
 
 
 if __name__ == "__main__":
