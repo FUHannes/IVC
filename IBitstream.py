b5076328090eba30e2ff9b9fd52ffd2683e2f71d
h4rrytrum4n <h4rrytrum4n@mi.fu-berlin.de>
2021-05-09 10:23:49 +0200

    Implement IBitstream.py

--- IBitstream.py
+++ IBitstream.py
@@ -1,29 +1,59 @@
+from bitarray import bitarray
+
+
 class IBitstream:
-    def __init__(self, filename):
+    def __init__(self, filename=None):
         self.filename = filename
-        pass
+        self._bitarray = None
+        self.read_file()
+
+    def read_file(self):
+        if self.filename:
+            with open(self.filename, 'rb') as fp:
+                self._bitarray = bitarray()
+                self._bitarray.fromfile(fp)
 
     def get_bit(self) -> int:
         """Reads a single bit from the bitstream
 
             :return next bit value 0 or 1
         """
-        pass
+        return self._bitarray.pop(0)
 
     def get_bits(self, num_bits: int) -> int:
         """Read multiple bits from the bitstream, most significant bit first
 
             :param      num_bits: Specifies the numbers of bits to read
             :return:    integer representation of bits read
-            :raises     ValueException: In case num_bit is less than zero
+            :raises     IndexError: if bitarray is empty or index is out of range
         """
-        if num_bits < 0:
-            raise ValueError('num_bits must be a non negative value')
-        pass
+        _int = int(self._bitarray[:num_bits].to01(), 2)
+        self._bitarray = self._bitarray[num_bits:]
+        return _int
 
     def num_available_bits(self) -> int:
         """
 
         :return: number of still available bits in the bitstream
         """
-        pass
+        return len(self._bitarray)
+
+    @property
+    def bitarray(self):
+        return self._bitarray
+
+
+if __name__ == '__main__':
+    i_bit_stream = IBitstream()
+    num_array = [166, 184]
+    _bit_array = bitarray()
+
+    for n in num_array:
+        _bit_array.frombytes(n.to_bytes(1, 'little'))
+
+    i_bit_stream._bitarray = _bit_array
+
+    print(i_bit_stream.get_bit(), i_bit_stream.bitarray.to01())  # returns 1 010011010111000
+    print(i_bit_stream.get_bits(5), i_bit_stream.bitarray.to01())  # returns 9 1010111000
+    print(i_bit_stream.get_bits(3), i_bit_stream.bitarray.to01())  # returns 5 0111000
+    print(i_bit_stream.get_bits(6), i_bit_stream.bitarray.to01())  # returns 28 0
