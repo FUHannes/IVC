36215f0af9249ac46b2a4663c9e54b8ee57b5863
paun51 <paun51@mi.fu-berlin.de>
2021-05-15 10:51:54 +0200

    [WIP] Remove redundant versions and runtime tests

--- IntraPredictionCalculator.py
+++ IntraPredictionCalculator.py
@@ -16,75 +16,9 @@ class IntraPredictionCalculator:
         else:
             raise Exception('Unsupported prediction mode')
 
-    def get_dc_predictionV1(self, x: int, y: int) -> int:
-        if x > 0 and y > 0:
-            left_mean = self.image[y:y+self.blocksize, x-1:x].mean()
-            top_mean  = self.image[y-1:y, x:x+self.blocksize].mean()
-            return round((left_mean + top_mean) / 2)
-        elif x > 0:
-            left_mean = self.image[y:y+self.blocksize, x-1:x].mean()
-            return round(left_mean)
-        elif y > 0:
-            top_mean  = self.image[y-1:y, x:x+self.blocksize].mean()
-            return round(top_mean)
-        else:
-            return 128
-
-    def get_dc_predictionV2(self, x: int, y: int) -> int:
+    def get_dc_prediction(self, x: int, y: int) -> int:
         left_border = self.image[y:y+self.blocksize, x-1:x].ravel() if x > 0 else []
-        top_border = self.image[y-1:y, x:x+self.blocksize].ravel() if y > 0 else []
+        top_border  = self.image[y-1:y, x:x+self.blocksize].ravel() if y > 0 else []
         border = np.concatenate([left_border, top_border])
         prediction = round(border.mean()) if border.size > 0 else 128
         return prediction
-
-    def get_dc_predictionV3(self, x: int, y: int) -> int:
-        left_border = []
-        if x > 0:
-            left_border = self.image[y:y+self.blocksize, x-1:x].ravel()
-        
-        top_border = []
-        if y > 0:
-            top_border = self.image[y-1:y, x:x+self.blocksize].ravel()
-        
-        border = np.concatenate([left_border, top_border])
-
-        prediction = 128
-        if border.size > 0:
-            prediction = round(border.mean())
-        
-        return prediction
-
-if __name__ == '__main__':
-    import time
-    import random
-
-    width = 1600
-    height = 1200
-    blocksize = 8
-    runden = 10
-
-    arr = np.random.uniform(0,256,(height,width)).astype('uint8')
-    tests = [(x, y) for y in range(0, height, blocksize) for x in range(0, width, blocksize)]
-    ipc = IntraPredictionCalculator(arr, blocksize)
-
-    def testFunction(name, func):
-        t0 = time.process_time()
-        for x, y in tests:
-            func(ipc, x, y)
-        td = time.process_time() - t0
-        return td
-
-    def measureFunction(name, func):
-        tda = [testFunction(name, func) for i in range(runden)]
-        tmean = np.mean(tda)
-        tstd = np.std(tda)
-        print(f'{name:2s}: mean={tmean:5f} std={tstd:5f}')
-
-    funcs = {
-        'V1': IntraPredictionCalculator.get_dc_predictionV1,
-        'V2': IntraPredictionCalculator.get_dc_predictionV2,
-        'V3': IntraPredictionCalculator.get_dc_predictionV3,
-    }
-
-    for k in funcs:
-        measureFunction(k, funcs[k])
